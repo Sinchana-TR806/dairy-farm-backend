32a94ee791ac00953a03c9446163d8ba9ba4581b
Sinchana-TR806 <Shalinigowdaa99@gmail.com>
2026-06-30 14:51:13 +0530

    Fix Render deployment

--- src/main/java/com/sridairy/DairyFarmApplication.java
+++ src/main/java/com/sridairy/DairyFarmApplication.java
@@ -7,22 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DairyFarmApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DairyFarmApplication.class, args);
-
-        try {
-            // Wait for backend startup
-            Thread.sleep(10000);
-
-            // Open frontend in default browser
-            Runtime.getRuntime().exec(
-                    "cmd /c start C:\\Users\\User\\Downloads\\index_final.html"
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         System.out.println("Sri Dairy Farm ERP Backend is running!");
-        System.out.println("API ready at: http://localhost:8080");
     }
 }
